f70d984e229d7e2fea64f18b6797ead4d237a4b7
Josh Comeau <joshwcomeau@gmail.com>
2019-05-09 09:42:23 -0400

    Add random color

--- src/components/LikeButton/ConfettiPiece.js
+++ src/components/LikeButton/ConfettiPiece.js
@@ -1,17 +1,21 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 import { random, range, sample } from '../../utils';
 
 import Particle from '../Particle';
 
 const ConfettiPiece = ({ parentSize, angle, distance, color }) => {
-  const size = parentSize * 0.1;
+  const size = parentSize * 0.25;
 
   return (
     <CenteredWithinParent>
-      <Particle angle={angle} distance={distance}>
+      <Particle
+        angle={angle}
+        startDistance={distance * 0.55}
+        endDistance={distance}
+      >
         <Circle
           style={{
             width: size,
