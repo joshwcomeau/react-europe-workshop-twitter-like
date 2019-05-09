f0d4b8ae670d34f75a9d618f7cb48cb73cd501d9
Josh Comeau <joshwcomeau@gmail.com>
2019-05-09 10:55:00 -0400

    Add LikeSprite

--- src/components/LikeButton/LikeButton.js
+++ src/components/LikeButton/LikeButton.js
@@ -1,43 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import PoppingCircle from '../PoppingCircle';
 import UnstyledButton from '../UnstyledButton';
-
-import ConfettiPiece from './ConfettiPiece';
-import ScaleIn from '../ScaleIn';
 import Heart from './Heart';
+import LikeSprite from '../LikeSprite';
 
 const PARTICLE_COLORS = ['#e53935', '#1e88e5', '#43a047', '#fdd835', '#fb8c00'];
 
 const LikeButton = ({ isLiked, numOfConfettiPieces = 1, size = 40 }) => {
   const heartSize = size * 0.6;
 
-  const heart = <Heart width={heartSize} isToggled={isLiked} />;
-
   return (
     <Wrapper style={{ width: size, height: size }}>
-      <Foreground>
-        {/* Part I: Update ScaleIn to do the springy scale effect */}
-        {isLiked ? <ScaleIn>{heart}</ScaleIn> : heart}
-      </Foreground>
-
-      <Background>
-        {/* Part II: Update the PoppingCircle */}
-        {isLiked && <PoppingCircle size={size} />}
-
-        {/* Part III: update ConfettiPiece, add several more: */}
-        {isLiked && (
-          <>
-            <ConfettiPiece
-              parentSize={size}
-              angle={40}
-              distance={25}
-              color={PARTICLE_COLORS[0]}
-            />
-          </>
-        )}
-      </Background>
+      <Heart width={heartSize} isToggled={isLiked} />
+      {isLiked && (
+        <SpriteWrapper>
+          <LikeSprite />
+        </SpriteWrapper>
+      )}
     </Wrapper>
   );
 };
@@ -49,14 +29,11 @@ const Wrapper = styled(UnstyledButton)`
   align-items: center;
 `;
 
-const Background = styled.div`
+const SpriteWrapper = styled.div`
   position: absolute;
-  z-index: 0;
-`;
-
-const Foreground = styled.div`
-  position: relative;
-  z-index: 1;
+  top: 0;
+  left: 0;
+  transform: translate(-10px, -10px);
 `;
 
 export default LikeButton;
---
+++ src/components/LikeSprite/LikeSprite.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
+
+import likeSpriteSrc from '../../assets/like-sprite.png';
+
+// The image was produced at
+
+const SCALE_RATIO = 2;
+
+const IMAGE_HEIGHT = 120 / SCALE_RATIO;
+const FRAME_SIZE = IMAGE_HEIGHT;
+const IMAGE_WIDTH = 11640 / SCALE_RATIO - FRAME_SIZE;
+const NUM_OF_FRAMES = 97 - 1;
+const SPEED = 60; // frames per second
+const ANIMATION_DURATION = (NUM_OF_FRAMES / SPEED) * 1000;
+
+const LikeSprite = () => {
+  return (
+    <Wrapper>
+      <Img src={likeSpriteSrc} />
+    </Wrapper>
+  );
+};
+
+const backgroundAnimation = keyframes`
+  to {
+    transform: translateX(-${IMAGE_WIDTH}px);
+  }
+`;
+
+const Wrapper = styled.div`
+  width: ${FRAME_SIZE}px;
+  height: ${FRAME_SIZE}px;
+  background: white;
+  overflow: hidden;
+  margin: auto;
+`;
+
+// prettier-ignore
+const Img = styled.img`
+  height: 100%;
+  animation:
+    ${backgroundAnimation}
+    ${ANIMATION_DURATION}ms
+    steps(${NUM_OF_FRAMES})
+    both;
+
+  will-change: transform;
+`;
+
+export default LikeSprite;
